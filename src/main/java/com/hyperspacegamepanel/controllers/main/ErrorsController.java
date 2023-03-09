4a0f3131f7024f97b13f5b9c2bddd8cd9164cc9f
Muhammad Haris <wannaheadshot5@gmail.com>
2023-03-09 17:22:11 +0500

    Shows `Errors` pages instead of white label page.

---
+++ src/main/java/com/hyperspacegamepanel/controllers/main/ErrorsController.java
@@ -0,0 +1,41 @@
+package com.hyperspacegamepanel.controllers.main;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class ErrorsController extends HelperController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("error") String error, Model m) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String returnTo = "";
+
+        if(status != null) {
+            Integer statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == 404) {
+                m.addAttribute("title", "404 Not Found | HyperSpaceGamePanel");
+                returnTo = "errors/404.html";
+            } else if(statusCode == 403) {
+                m.addAttribute("title", "403 Forbidden | HyperSpaceGamePanel");
+                returnTo = "errors/403.html";
+            } else {
+                m.addAttribute("title", "Internal Server Error | HyperSpaceGamePanel");
+                returnTo = "errors/500.html";
+            }
+        }
+        
+        return returnTo;
+
+    }
+    
+}
