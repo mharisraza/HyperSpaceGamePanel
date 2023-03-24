b84eb0d50aea25bc8edf987d855b1f6da1176f05
Muhammad Haris <wannaheadshot5@gmail.com>
2023-03-24 07:38:22 +0500

    refactor `SupportController`

--- src/main/java/com/hyperspacegamepanel/controllers/user/SupportController.java
+++ src/main/java/com/hyperspacegamepanel/controllers/user/SupportController.java
@@ -1,55 +1,48 @@
 package com.hyperspacegamepanel.controllers.user;
 
 import java.security.Principal;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.hyperspacegamepanel.controllers.main.HelperController;
+import com.hyperspacegamepanel.exceptions.ResourceNotFound;
+import com.hyperspacegamepanel.helper.Alert;
 import com.hyperspacegamepanel.models.ticket.Ticket;
 import com.hyperspacegamepanel.models.ticket.TicketReply;
 import com.hyperspacegamepanel.models.user.User;
-import com.hyperspacegamepanel.repositories.TicketReplyRepository;
-import com.hyperspacegamepanel.repositories.TicketRepository;
-import com.hyperspacegamepanel.repositories.UserRepository;
 import com.hyperspacegamepanel.services.TicketService;
 
 @Controller
 @RequestMapping("/me/support")
-public class SupportController extends UserController {
+public class SupportController extends HelperController {
 
     @Autowired
     private TicketService ticketService;
-
-    @Autowired
-    private UserRepository userRepo;
-
-    @Autowired
-    private TicketRepository ticketRepo;
-
-    @Autowired
-    private TicketReplyRepository ticketReplyRepo;
 
     @Autowired
     private HttpSession httpSession;
 
     // showing all the user's ticket.
     @GetMapping("/tickets")
-    public String userTickets(Model m, Principal principal) {
+    public String userTickets(Model m, Principal principal) throws InterruptedException, ExecutionException {
         User user = getLoggedInUser(principal);
-        m.addAttribute("tickets", user.getTickets());
+        m.addAttribute("tickets", this.ticketService.getAllTicketsOfUser(user.getId()).join());
         m.addAttribute("title", "Your Tickets | HyperSpaceGamePanel");
         return "user/tickets";
     }
@@ -62,143 +55,137 @@ public class SupportController extends UserController {
         return "user/new_ticket";
     }
 
-    // handling logic where user can delete additional ticket message or close the
-    // ticket.
-    @GetMapping("")
-    public String ticket(@RequestParam(required = false) Integer ticketId, Principal principal, Model m,
-            @RequestParam(required = false) String action, @RequestParam(required = false) Integer messageId) {
-        if (ticketId == null) {
-            httpSession.setAttribute("status", "CANT_FIND_TICKET");
-            return "redirect:/me/support/tickets";
-        }
+    @GetMapping("/ticket/view/{ticketId}")
+    public String ticket(@PathVariable Integer ticketId, Principal principal, Model m, @RequestParam(required = false) String action, @RequestParam(required = false) Integer messageId) {
+        if (ticketId == null) return "redirect:/me/support/tickets";
 
-        User user = getLoggedInUser(principal);
-        Optional<Ticket> ticket = this.ticketRepo.findById(ticketId);
+        Ticket ticket = null;
+        List<TicketReply> ticketReplies = null;
 
-        if (!ticket.isPresent() || ticket.get().getUser().getId() != user.getId()) {
-            httpSession.setAttribute("status", "CANT_FIND_TICKET");
-            return "redirect:/me/support/tickets";
-        }
+        if(action != null) {
 
-        List<TicketReply> ticketReplies = this.ticketReplyRepo.findAll();
+            switch(action) {
+                
+                case "closeTicket" -> {
+                     try {
+                        this.ticketService.closeTicket(ticketId);
+                        httpSession.setAttribute("status", new Alert("Ticket closed successfully.", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+                        return "redirect:/me/support/ticket/view/"+ticketId;
+                    } catch (Exception e) {
+                        httpSession.setAttribute("status", new Alert("Sorry, we were unable to close the ticket at this moment, please try later", Alert.ERROR, Alert.ERROR_CLASS));
+                        return "redirect:/me/support/ticket/view/"+ticketId;
+                    }
+                }
 
-        if (action != null && action.equals("closeTicket")) {
-            ticket.get().setClosed(true);
-            ticket.get().setClosedBy("USER");
-            ticket.get().setStatus("Closed");
-            httpSession.setAttribute("status", "TICKET_CLOSED_SUCCESSFULLY");
-            this.ticketRepo.save(ticket.get());
-            return "redirect:/me/support?ticketId=" + ticketId;
-        }
+                case "uncloseTicket" -> {
+                    try {
+                      this.ticketService.uncloseTicket(ticketId, getLoggedInUser(principal).getId());
+                      httpSession.setAttribute("status", new Alert("Ticket unclosed successfully.", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+                      return "redirect:/me/support/ticket/view/"+ticketId;
+                    } catch (Exception e) {
+                        if(e.getMessage().equals("UNABLE_TO_UNCLOSE_THE_TICKET[REASON:TICKET_CLOSED_BY_ADMIN]")) {
+                            httpSession.setAttribute("status", new Alert("This ticket was closed by an admin, it cannot unclose by you.", Alert.ERROR, Alert.ERROR_CLASS));
+                            return "redirect:/me/support/ticket/view/"+ticketId;
+                        }
+                        httpSession.setAttribute("status", new Alert("Sorry, we were unable to un-close the ticket at this moment, please try later.", Alert.ERROR, Alert.ERROR_CLASS));
+                        return "redirect:/me/support/ticket/view/"+ticketId;
+                    }
+                }
 
-        if (action != null && action.equals("uncloseTicket")) {
+                case "deleteMessage" -> {
+                    try {
+                           this.ticketService.deleteTicketReply(messageId, false);
+                           httpSession.setAttribute("status", new Alert("Successfully delete the message reply.", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+                           return "redirect:/me/support/ticket/view/"+ticketId;
+                    } catch (Exception e) {
+                        if(e.getMessage().equals("CANNOT_DELETE_THE_REPLY_MESSAGE_BECAUSE_IT_IS_SEND_BY_ADMIN")) {
+                            httpSession.setAttribute("status", new Alert("Cannot delete the reply, it was send by an admin.", Alert.ERROR, Alert.ERROR_CLASS));
+                            return "redirect:/me/support/ticket/view/" + ticketId;
+                        }
+                        httpSession.setAttribute("status", new Alert("Unabled to delete the ticket reply.", Alert.ERROR, Alert.ERROR_CLASS));
+                        return "redirect:/me/support/ticket/view/" + ticketId;
+                    }
+                }
 
-            if(ticket.get().getClosedBy() == "ADMIN") {
-                httpSession.setAttribute("status", "CANT_UNCLOSE_TICKET_CLOSED_BY_ADMIN");
-                return "redirect:/me/support?ticketId="+ticketId;
+                default -> {
+                    httpSession.setAttribute("status", new Alert("Invalid request or action.", Alert.ERROR, Alert.ERROR_CLASS));
+                    return "redirect:/me/support/ticket/view/"+ticketId;
+                }
             }
-
-            ticket.get().setClosed(false);
-            ticket.get().setStatus("Ticket is recently unclosed.");
-            httpSession.setAttribute("status", "TICKET_UNCLOSED_SUCCESSFULLY");
-            this.ticketRepo.save(ticket.get());
-            return "redirect:/me/support?ticketId=" + ticketId;
         }
 
-        if(action != null && action.equals("deleteMessage")) {
-            if(messageId == null) {
-                return "redirect:/me/support?ticketId=" + ticketId;
-            }
-
-         Optional<TicketReply> ticketReply = this.ticketReplyRepo.findById(messageId);
-
-         if(!ticketReply.isPresent() || ticketReply.get().getSender().getId() != user.getId()  || ticketReply.get().getSender().getRole() == User.ROLE_ADMIN) {
-            httpSession.setAttribute("status", "TICKET_REPLY_DOES_NOT_EXIST_OR_NOT_ALLOW_TO_DELETE");
-            return "redirect:/me/support?ticketId=" + ticketId;
-         }
-              
-            this.ticketReplyRepo.delete(ticketReply.get());
-            httpSession.setAttribute("status", "REPLY_MESSAGE_DELETED_SUCCESSFULLY");
-            return "redirect:/me/support?ticketId=" + ticketId;
+        try {
+         ticket = this.ticketService.getTicketByUser(ticketId, getLoggedInUser(principal).getId()).join();
+         ticketReplies = this.ticketService.getTicketRepliesByTicket(ticket.getId()).join();
+        } catch (Exception e) {
+             if(e instanceof ResourceNotFound && e.getMessage().contains("Ticket not found")) {
+                httpSession.setAttribute("status", new Alert("Sorry, we were unable to found the ticket. it may doesn't exist or deleted.", Alert.ERROR, Alert.ERROR_CLASS));
+                return "redirect:/me/support/tickets";
+             }
         }
-
         m.addAttribute("ticketReplies", ticketReplies);
-        m.addAttribute("ticket", ticket.get());
-        m.addAttribute("title", ticket.get().getSubject() + " | HyperSpaceGamePanel");
+        m.addAttribute("ticket", ticket);
+        m.addAttribute("title", ticket.getSubject() + " | HyperSpaceGamePanel");
         return "user/ticket";
     }
 
     // processing and handling the newly submitted ticket.
     @PostMapping("/submit-ticket")
-    public String processTicket(@Valid @ModelAttribute Ticket ticket, BindingResult bindResult, Model m,
-            @RequestParam Integer userId) {
-
-        if (bindResult.hasErrors()) {
-            m.addAttribute("ticket", ticket);
-            return "user/new_ticket";
+    public String processTicket(@Valid @ModelAttribute Ticket ticket, BindingResult bindResult, Model m, @RequestParam Integer userId) {
+        if (bindResult.hasErrors()) return "user/new_ticket";
+            
+        Integer createdTicketId = null;
+        try {
+            Ticket createdTicket = this.ticketService.createTicket(ticket, userId).join();
+            createdTicketId = createdTicket.getId();
+        } catch (Exception e) {
+            httpSession.setAttribute("status", new Alert("Ahh sorry, cannot submitted the ticket, please try again.", Alert.ERROR, Alert.ERROR_CLASS));
+            return "redirect:/me/support/tickets";
         }
-
-        User user = this.userRepo.findById(userId).get();
-        ticket.setUser(user);
-        ticket.setSubmittedDate(new Date());
-        ticket = this.ticketService.createTicket(ticket);
-
-        if (ticket != null) {
-            httpSession.setAttribute("status", "TICKET_SUBMITTED_SUCCESSFULLY");
-            return "redirect:/me/support?ticketId=" + ticket.getId();
-        }
-
-        httpSession.setAttribute("status", "SOMETHING_WENT_WRONG");
-        return "user/new_ticket";
+        return "redirect:/me/support/ticket/view/" + createdTicketId;
     }
 
     // processing and handling reply message to the existing ticket.
     @PostMapping("/process-reply")
-    public String processAdditionalMessage(@RequestParam Integer ticketId, @RequestParam String replyMessage,
-            Principal principal) {
+    public String processAdditionalMessage(@RequestParam Integer ticketId, @RequestParam String replyMessage, Principal principal) {
 
         // if ticket id is empty we're not allowing to proceed.
         if (ticketId == null) {
-            httpSession.setAttribute("status", "CANT_FIND_TICKET");
-            return "redirect:/me/support/tickets";
-        }
-
-        // getting logged in user info and ticket with provided ticket id.
-        User user = getLoggedInUser(principal);
-        Optional<Ticket> ticket = this.ticketRepo.findById(ticketId);
-
-        // may be ticketId were manipulated or ticket doesn't belong to the logged in
-        // user's ticket.
-        if (!ticket.isPresent() || ticket.get().getUser().getId() != user.getId()) {
-            httpSession.setAttribute("status", "CANT_FIND_TICKET");
+            httpSession.setAttribute("status", new Alert("Ahh sorry, cannot find the ticket to submit the reply, please try again.", Alert.ERROR, Alert.ERROR_CLASS));
             return "redirect:/me/support/tickets";
         }
 
         // returns if message is empty.
         if (replyMessage == null || replyMessage.isBlank()) {
-            httpSession.setAttribute("status", "REPLY_MESSAGE_IS_EMPTY");
-            return "redirect:/me/support?ticketId=" + ticketId;
+            httpSession.setAttribute("status", new Alert("Message is required for reply to the submitted ticket.", Alert.ERROR, Alert.ERROR_CLASS));
+            return "redirect:/me/support/ticket/view/" + ticketId;
         }
 
-        // returns if ticket is closed and code is manipulated.
-        if (ticket.get().isClosed()) {
-            httpSession.setAttribute("status", "CANT_ADD_MESSAGE_TICKET_IS_CLOSED");
-            return "redirect:/me/support?ticketId=" + ticketId;
+        // getting logged in user
+        User user = getLoggedInUser(principal);
+
+        try {
+            Ticket ticket = this.ticketService.getTicketByUser(ticketId, user.getId()).join();
+
+            TicketReply ticketReply = new TicketReply();
+            ticketReply.setMessage(replyMessage);
+            ticketReply.setSender(user);
+            ticketReply.setTicket(ticket);
+
+            ticketReply = this.ticketService.createTicketReply(ticketReply, ticket).join();
+
+            // if success.
+            httpSession.setAttribute("status", new Alert("Message added successfully.", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+
+        } catch (Exception e) {
+            if (e instanceof ResourceNotFound && e.getMessage().contains("Ticket not found")) {
+                httpSession.setAttribute("status", new Alert("Sorry, cannot submit the reply to the ticket, ticket doesn't exist or deleted.", Alert.ERROR, Alert.ERROR_CLASS));
+                return "redirect:/me/support/ticket/view/" + ticketId;
+            }
+            httpSession.setAttribute("status", new Alert("Ahh sorry, cannot submitted the ticket, please try later.", Alert.ERROR, Alert.ERROR_CLASS));
         }
 
-        TicketReply ticketReply = new TicketReply();
-        ticketReply.setRepliedDate(new Date());
-        ticketReply.setTicket(ticket.get());
-        ticketReply.setSender(user);
-        ticketReply.setMessage(replyMessage);
-        ticket.get().setStatus("Pending Admin Response");
-
-
-        this.ticketReplyRepo.save(ticketReply);
-
-        httpSession.setAttribute("status", "REPLY_MESSAGE_ADDED_SUCCESSFULLY");
-
-        return "redirect:/me/support?ticketId=" + ticketId;
+        return "redirect:/me/support/ticket/view/" + ticketId;
     }
 
 }
