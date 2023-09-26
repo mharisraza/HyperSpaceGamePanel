bb93ac6ce67479e45d90e7392d1490d79dd948c8
Muhammad Haris <wannaheadshot5@gmail.com>
2023-09-26 19:32:22 +0500

    refactor admin role checker interceptor

--- src/main/java/com/hyperspacegamepanel/interceptors/AdminRoleCheckerInterceptor.java
+++ src/main/java/com/hyperspacegamepanel/interceptors/AdminRoleCheckerInterceptor.java
@@ -1,16 +1,14 @@
 package com.hyperspacegamepanel.interceptors;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.hyperspacegamepanel.models.user.User;
+import com.hyperspacegamepanel.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.hyperspacegamepanel.models.user.User;
-import com.hyperspacegamepanel.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Service
 public class AdminRoleCheckerInterceptor implements HandlerInterceptor {
@@ -19,16 +17,22 @@ public class AdminRoleCheckerInterceptor implements HandlerInterceptor {
     private UserRepository userRepo;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // Check if the "redirection" attribute already exists in the session
+        if (request.getSession().getAttribute("redirection") == null) {
+            List<User> users = this.userRepo.findAllByRole("ROLE_ADMIN");
 
-        List<User> users = this.userRepo.findAllByRole("ROLE_ADMIN");
+            System.out.println("Number of admin users: " + users.size());
+            System.out.println("USERS IS EMPTY OR NOT: " + users.isEmpty());
 
-        if (users.isEmpty()) {
-            request.getSession().setAttribute("redirection", "/addAdmin");
+            if (users.isEmpty()) {
+                // Set the "redirection" attribute if there are no admin users
+                request.getSession().setAttribute("redirection", "/addAdmin");
+            }
         }
 
         return true;
     }
+
 
 }
