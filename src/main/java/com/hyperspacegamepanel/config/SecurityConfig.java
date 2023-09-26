5db65d2d1ba92b421a6aaa0012edec6d857f22b8
Muhammad Haris <wannaheadshot5@gmail.com>
2023-09-26 19:12:01 +0500

    made spring security changes for 3.1.4

--- src/main/java/com/hyperspacegamepanel/config/SecurityConfig.java
+++ src/main/java/com/hyperspacegamepanel/config/SecurityConfig.java
@@ -1,41 +1,33 @@
 package com.hyperspacegamepanel.config;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.hyperspacegamepanel.models.user.User;
+import com.hyperspacegamepanel.repositories.UserRepository;
+import com.hyperspacegamepanel.utils.Alert;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import com.hyperspacegamepanel.helper.Alert;
-import com.hyperspacegamepanel.models.user.User;
-import com.hyperspacegamepanel.repositories.UserRepository;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SecurityConfig {
 
-    @Autowired
-    private UserRepository userRepo;
+    @Autowired private UserRepository userRepo;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -52,104 +44,72 @@ public class SecurityConfig {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(this.passwordEncoder());
         daoAuthenticationProvider.setUserDetailsService(this.userDetailsService());
-        daoAuthenticationProvider.setPostAuthenticationChecks(new UserDetailsChecker() {
-
-            @Override
-            public void check(UserDetails toCheck) {
-                if(!((CustomUserDetails) toCheck).isVerified()) {
-                    throw new DisabledException("USER_IS_NOT_VERIFIED");
-                }
+        daoAuthenticationProvider.setPostAuthenticationChecks(toCheck -> {
+            if(!((CustomUserDetails) toCheck).isVerified()) {
+                throw new DisabledException("USER_IS_NOT_VERIFIED");
             }
-            
         });
         return daoAuthenticationProvider;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
-         http.authorizeHttpRequests()
-        .antMatchers("/admin/**")
-        .hasRole("ADMIN")
-        .antMatchers("/me/**")
-        .hasRole("USER")
-        .antMatchers("/**")
-        .permitAll()
-        .and()
-        .formLogin()
-        .successHandler(new AuthenticationSuccessHandler() {
-                        // do actions if user login successfully.
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                    Authentication authentication) throws IOException, ServletException {
-
-                        request.getSession().setAttribute("status", new Alert("Login successfully.", Alert.SUCCESS, Alert.SUCCESS_CLASS));           
-                        User user = userRepo.getByEmail(authentication.getName());
-                        String redirectURL = "";
-
-                        if(user.getRole().equals(User.ROLE_ADMIN)) {
-                            redirectURL = "/admin/dashboard";
-                        }
-                        if(user.getRole().equals(User.ROLE_USER)) {
-                            redirectURL = "/me/dashboard";
-                        }
-
-                        response.sendRedirect(redirectURL);
-
-            }
-        })
-        .failureHandler(new AuthenticationFailureHandler() {
-
-            // do actions if user can't login.
-            // REASON: user is disabled or bad credentials.
-            
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-                    AuthenticationException exception) throws IOException, ServletException {
-
-                        if(exception instanceof DisabledException && exception.getMessage().equals("USER_IS_NOT_VERIFIED")) {
-                            request.getSession().setAttribute("status", new Alert("Your account is not verified, please verify to login.", Alert.ERROR, Alert.ERROR_CLASS));
-                            response.sendRedirect("/verifyAccount?");
-                            return;
-                        }
-                    
-                        if(exception.getClass().isAssignableFrom(DisabledException.class)) {
-                            request.getSession().setAttribute("status", new Alert("Your Account is suspended, please contact admin to unblock.", Alert.ERROR, Alert.ERROR_CLASS));
-                            response.sendRedirect("/login?=AccountSuspended");
-                            return;
-                        }
-
-                        if(exception.getClass().isAssignableFrom(BadCredentialsException.class)) {
-                            request.getSession().setAttribute("status", new Alert("The username, email, or password you entered is incorrect.", Alert.ERROR, Alert.ERROR_CLASS));
-                            response.sendRedirect("/login?=BadCredentials");
-                            return;
-                        }
-            }
-        })
-        .loginPage("/login")
-        .and()
-        .logout()
-        .logoutSuccessHandler(new LogoutSuccessHandler() {
-                     // do actions if user logout successfully
-            @Override
-            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
-                    Authentication authentication) throws IOException, ServletException {
-                        if(authentication != null) {
-                            request.getSession().setAttribute("status", new Alert("Logout successfully", Alert.SUCCESS, Alert.SUCCESS_CLASS));
-                            response.sendRedirect("/login?s=logout");
-                        } else {
-                            response.sendRedirect("/login?e");
-                        }
-            }
-            
-        })
-        .and()
-        .csrf()
-        .disable();
-
-      
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/panel/**").hasRole("USER")
+                                .requestMatchers("/**").permitAll()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .successHandler(this::handleSuccessfulLogin)
+                                .failureHandler(this::handleFailedLogin)
+                                .loginPage("/login")
+                )
+                .logout(logout ->
+                        logout.logoutSuccessHandler(this::handleLogoutSuccess)
+                )
+                .build();
     }
+
+    private void handleSuccessfulLogin(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        request.getSession().setAttribute("status", new Alert("Login successfully.", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+        User user = userRepo.getByEmail(authentication.getName());
+        String redirectURL = "";
+
+        if (user.getRole().equals(User.ROLE_ADMIN)) {
+            redirectURL = "/admin/dashboard";
+        }
+        if (user.getRole().equals(User.ROLE_USER)) {
+            redirectURL = "/me/dashboard";
+        }
+
+        response.sendRedirect(redirectURL);
+    }
+
+    private void handleFailedLogin(HttpServletRequest request, HttpServletResponse response, Exception exception) throws IOException {
+        if (exception instanceof DisabledException && exception.getMessage().equals("USER_IS_NOT_VERIFIED")) {
+            request.getSession().setAttribute("status", new Alert("Your account is not verified, please verify to login.", Alert.ERROR, Alert.ERROR_CLASS));
+            response.sendRedirect("/verifyAccount?");
+        } else if (exception.getClass().isAssignableFrom(DisabledException.class)) {
+            request.getSession().setAttribute("status", new Alert("Your Account is suspended, please contact admin to unblock.", Alert.ERROR, Alert.ERROR_CLASS));
+            response.sendRedirect("/login?=AccountSuspended");
+        } else if (exception.getClass().isAssignableFrom(BadCredentialsException.class)) {
+            request.getSession().setAttribute("status", new Alert("The username, email, or password you entered is incorrect.", Alert.ERROR, Alert.ERROR_CLASS));
+            response.sendRedirect("/login?=BadCredentials");
+        }
+    }
+
+    private void handleLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        if (authentication != null) {
+            request.getSession().setAttribute("status", new Alert("Logout successfully", Alert.SUCCESS, Alert.SUCCESS_CLASS));
+            response.sendRedirect("/login?s=logout");
+        } else {
+            response.sendRedirect("/login?e");
+        }
+    }
+
 
 }
